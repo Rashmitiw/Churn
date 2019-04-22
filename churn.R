923c6e5dd27fa86030d20f78cef166b021a5e162
Rashmi Tiwari <tiwari110892@gmail.com>
2019-04-22 22:38:56 +0530

    Update churn.R
    AUC score 90.91

--- churn.R
+++ churn.R
@@ -2,6 +2,8 @@
 library(caret)
 library(caTools)
 library(pROC)
+
+#read table 
 churn<-read.csv("churn.csv",header = T,stringsAsFactors = F,na.strings = "?")
 str(churn)
 
@@ -35,7 +37,7 @@ str(churn)
 churn1<-churn[-1] 
 str(churn1)
 
-
+##remove missing value
 
 #remove NA
 
@@ -44,11 +46,15 @@ library(VIM)
 churn2<-kNN(churn1)
 summary(churn2)
 str(churn2)
+
+
 churn3<-subset(churn2,select=1:21)
 summary(churn3)
 str(churn3)
 
 set.seed(12)
+
+#remove variable from table that are not very significant in churn
 
 churnrf<-churn3[,-c(1,3,4,6,7,8,9,11,14,15,17,19)] #90.91
 
@@ -58,20 +64,26 @@ trainC<-churnrf[ind,]
 testC<-churnrf[-ind,]
 str(trainC)
 
+##Control statement for cross validation 
+
 Control<-trainControl(method="cv",number=10,savePredictions = TRUE,classProbs = TRUE)
 
-
-
-
-
-#Random Forest
+#train mmodelRandom Forest with 10 k fold cross validation 
 
 RF<-train(Churn~.,data=trainC,method="rf",parms = list(split = "information"),
           trControl=Control)
 summary(RF)
 prediction3<-predict(RF,testC)
+
+##prediction in probability
+
 PredictionWithProbs3<-predict(RF,testC,type="prob")
+
+#AUC score 
+
 auc<-auc(testC$Churn,PredictionWithProbs3[,2])
 auc
-confusionMatrix(prediction3,testC$Churn) 
 
+#Confusion Matrix 
+
+confusionMatrix(prediction3,testC$Churn) 
